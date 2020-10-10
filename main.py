c9f7574cf04658029450cbadc62732dada649c86
Grant Pittman <grantpittman@Grants-MBP.attlocal.net>
2020-10-10 13:57:34 -0500

    adding filters to web app and making it pretty

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import numpy as np
 import pandas as pd
 
 st.title("FIFA 2018 Player Skills")
+
+img = "media/FIFA.jpg"
+
+st.image(img, width = 400)
+
 st.markdown(
 """
 This is a project that uses ML to determine player pay based on their differnt skill levels
@@ -14,11 +19,24 @@ def load_data(nrows):
     data = pd.read_csv(data_location, encoding = 'latin1', nrows=nrows)
     return data
 
-data_points = st.slider('data points', 0, 100, 50)
+if st.checkbox('Show raw data'):
+    with st.beta_container():
+        data_points = st.slider('data points', 0, 100, 50)
+        data = load_data(data_points)
+        st.subheader('Raw data')
+        st.dataframe(data)
 
-data = load_data(data_points)
 
+MLoption = st.sidebar.selectbox(
+'What method of Unsupervised Learning do you want to use?',
+('K-Means', 'Gaussian Mixture'))
 
-st.write("Here's our first attempt at using data to create a table:")
-st.dataframe(data)
+country_select = st.sidebar.text_input(
+    'Which country are you interested in?',
+    value = "Spain"
+)
 
+club_select = st.sidebar.text_input(
+    'Which club are you interested in?',
+    value = "Real Madrid"
+)
